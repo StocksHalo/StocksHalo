7409f38872d4feed6685fda11f2654e1e32a79d4
Yuvraj Singh Deol <80378659+YuVrAj888@users.noreply.github.com>
2021-05-11 22:28:20 +0530

    Update routes.py

--- flaskfromscratch/flaskfromscratch/routes.py
+++ flaskfromscratch/flaskfromscratch/routes.py
@@ -54,12 +54,12 @@ def index():
 
 @app.route("/")
 @app.route("/home")
-@app.route("/blog")
+@app.route("/insight")
 
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('home.html', posts=posts, title='Blog')
+    return render_template('home.html', posts=posts, title='Insight')
 
 
 
