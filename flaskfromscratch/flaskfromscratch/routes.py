a07495a67bd8b14d6313d0fbc7610a456cfca67d
Akshay Kumar <akshaykumarragavan@gmail.com>
2021-05-12 13:27:29 +0400

    Integrated Calendar

--- flaskfromscratch/flaskfromscratch/routes.py
+++ flaskfromscratch/flaskfromscratch/routes.py
@@ -381,3 +381,73 @@ def news():
 
 
     return render_template('news.html', context=mylist, title='News')
+
+
+events = [
+  {
+    'todo' : 'Id-Ul-Fitr (Ramzan Id)',
+    'date' : '2021-05-14',
+  },
+  {
+    'todo' : 'Republic Day',
+    'date' : '2021-01-26',
+  },
+   {
+    'todo' : 'Mahashivratri',
+    'date' : '2021-03-11',
+  },
+   {
+    'todo' : 'Holi',
+    'date' : '2021-03-29',
+  },
+   {
+    'todo' : 'Good Friday',
+    'date' : '2021-04-02',
+  },
+   {
+    'todo' : 'Dr.Baba Saheb Ambedkar Jayanti',
+    'date' : '2021-04-14',
+  },
+   {
+    'todo' : 'Ram Navami',
+    'date' : '2021-04-21',
+  },
+   {
+    'todo' : 'Bakri Id',
+    'date' : '2021-07-21',
+  },
+   {
+    'todo' : 'Muharram',
+    'date' : '2021-08-19',
+  },
+   {
+    'todo' : 'Ganesh Chaturthi',
+    'date' : '2021-09-10',
+  },
+   {
+    'todo' : 'Dussehra',
+    'date' : '2021-10-15',
+  },
+   {
+    'todo' : 'Diwali Balipratipada',
+    'date' : '2021-11-05',
+  },
+   {
+    'todo' : 'Christmas',
+    'date' : '2021-12-26',
+  },
+   {
+    'todo' : 'Gurunanak Jayanti',
+    'date' : '2021-11-19',
+  },
+   {
+    'todo' : 'Independence Day',
+    'date' : '2021-08-15',
+  },
+]
+
+@app.route('/calendar')
+def calendar():
+
+
+    return render_template('calendar.html', events=events ,title='News')
