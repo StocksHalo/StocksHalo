b2bd992289ab9baa0017b1ad04adc88c44998d96
Yuvraj Singh Deol <80378659+YuVrAj888@users.noreply.github.com>
2021-05-13 22:39:53 +0530

    Update buy_stock.js

--- flaskfromscratch/flaskfromscratch/static/buy_stock.js
+++ flaskfromscratch/flaskfromscratch/static/buy_stock.js
@@ -1,15 +1,15 @@
 window.onload = function() {
 
     // Get references to elements on the page.
-    var form1 = document.getElementById('quantity-buy-form');
-    var form2 = document.getElementById('quantity-sell-form');
-    var quantityCaptured1 = document.getElementById('buy-qty');
-    var quantityCaptured2 = document.getElementById('sell-qty');
-    var ptagId = document.getElementById('transaction-history');
-    var pbalanceId = document.getElementById('current-balance');
-    var pquantityId = document.getElementById('current-quantity');
-    var stockPriceBuy = document.getElementById('current-stock-price-buy')
-    var stockPriceSell = document.getElementById('current-stock-price-sell')
+    //var form1 = document.getElementById('quantity-buy-form');
+    //var form2 = document.getElementById('quantity-sell-form');
+    //var quantityCaptured1 = document.getElementById('buy-qty');
+    //var quantityCaptured2 = document.getElementById('sell-qty');
+    //var ptagId = document.getElementById('transaction-history');
+    //var pbalanceId = document.getElementById('current-balance');
+    //var pquantityId = document.getElementById('current-quantity');
+    //var stockPriceBuy = document.getElementById('current-stock-price-buy')
+    //var stockPriceSell = document.getElementById('current-stock-price-sell')
   
     form1.onsubmit = function(e) {
         alert("Market Closed: Can't Place Order");
